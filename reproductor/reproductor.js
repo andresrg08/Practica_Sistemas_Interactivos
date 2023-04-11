d8b23b8ac9d375b03deac02b235f2f963716a92d
Joaquín García Pozo <100451183@alumnos.uc3m.es>
2023-04-11 19:15:32 +0200

    Inicio de la práctica. Se ha realizado el html del reproductor

--- reproductor/reproductor.js
+++ reproductor/reproductor.js
@@ -1,1 +1,10 @@
-a
+function mostrarVideo(idVideo) {
+    // Ocultar todos los videos
+    var videos = document.getElementsByClassName('video');
+    for (var i = 0; i < videos.length; i++) {
+      videos[i].style.display = 'none';
+    }
+    // Mostrar el video correspondiente
+    var videoMostrado = document.getElementById(idVideo);
+    videoMostrado.style.display = 'block';
+  }
