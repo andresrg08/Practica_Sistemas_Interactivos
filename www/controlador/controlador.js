6efa8a30535a3c491e6dfabf6e6da70ba91f4b7f
andresrg2 <128091982+andresrg08@users.noreply.github.com>
2023-04-19 21:12:23 +0200

    mas cambios implementados

--- www/controlador/controlador.js
+++ www/controlador/controlador.js
@@ -3,35 +3,38 @@ var socket = io();
 socket.on("connect", function(){
   socket.emit("conexion establecida");
 
-  var pausaBtn = document.getElementById('esfera');
+  var joystick = document.getElementById('esfera');
   var initialX, initialY;
   var currentX = -200, currentY = -200;
 
   // Eventos táctiles para dispositivos móviles
-  pausaBtn.addEventListener('touchstart', function(event) {
+  joystick.addEventListener('touchstart', function(event) {
     initialX = event.touches[0].clientX - currentX;
     initialY = event.touches[0].clientY - currentY;
   });
 
-  pausaBtn.addEventListener('touchmove', function(event) {
+  joystick.addEventListener('touchmove', function(event) {
     event.preventDefault();
     currentX = event.touches[0].clientX - initialX;
     currentY = event.touches[0].clientY - initialY;
+    console.log(currentX)
    
-    pausaBtn.style.transform = "translate(" + currentX + "px, " + currentY + "px)";
+    joystick.style.transform = "translate(" + currentX + "px, " + currentY + "px)";
   });
 
-  pausaBtn.addEventListener('touchend', function() {
+  joystick.addEventListener('touchend', function() {
     console.log("Mover esfera");
     socket.emit('mover_esfera', {x: currentX, y: currentY});
     currentX = -200;
     currentY = -200;
-    pausaBtn.style.transform = "translate(" + currentX + "px, " + currentY + "px)";
+    joystick.style.transform = "translate(" + currentX + "px, " + currentY + "px)";
   });
 
   // Evento de clic para navegadores de escritorio
-  pausaBtn.addEventListener('click', function() {
+  joystick.addEventListener('click', function() {
     console.log("Tocar esfera");
     socket.emit('pausa', true);
   });
+
+  
 });
